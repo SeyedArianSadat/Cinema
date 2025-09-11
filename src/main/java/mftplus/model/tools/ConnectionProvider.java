10a398f10edb6e73b5ac500b17d1321f35d3ec23
seyedariansadat <ariansadat733@gmail.com>
2025-09-11 13:05:02 +0330

    update bring log

---
+++ src/main/java/mftplus/model/tools/ConnectionProvider.java
@@ -0,0 +1,40 @@
+package mftplus.model.tools;
+
+import lombok.Getter;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ConnectionProvider {
+    @Getter
+    private final static ConnectionProvider provider = new ConnectionProvider();
+
+    private ConnectionProvider() {
+    }
+
+    public Connection getOracleConnection() throws SQLException {
+        return DriverManager.getConnection(
+                "jdbc:oracle:thin:@localhost:1521:XE",
+                "javase",
+                "java123"
+        );
+    }
+
+    public int getNextId(String sequenceName) throws Exception{
+
+        ResultSet resultSet = getOracleConnection().prepareStatement(String.format("select %s.nextval as NEXT_ID from dual", sequenceName)).executeQuery();
+        resultSet.next();
+        return resultSet.getInt("NEXT_ID");
+    }
+
+    public Connection getH2Connection() throws SQLException {
+//        Class.forName("org.h2.Driver");
+        return DriverManager.getConnection(
+                "jdbc:h2:file:./db/mft_class;AUTO_SERVER=TRUE;MODE=Oracle",
+                "sa",
+                ""
+        );
+    }
+}
