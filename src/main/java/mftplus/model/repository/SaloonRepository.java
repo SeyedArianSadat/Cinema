142f358e39201c2f8650ac5eeb6c679c0ebb0b3e
romina <rominamirfattahi13@gmail.com>
2025-09-16 20:18:17 +0330

    romina's files added

--- src/main/java/mftplus/model/repository/SaloonRepository.java
+++ src/main/java/mftplus/model/repository/SaloonRepository.java
@@ -1,4 +1,95 @@
 package mftplus.model.repository;
 
-public class SaloonRepository {
+import mftplus.model.entity.Payment;
+import mftplus.model.entity.Saloon;
+import mftplus.model.tools.ConnectionProvider;
+import mftplus.model.tools.SaloonMapper;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SaloonRepository implements Repository<Saloon, Integer> , AutoCloseable {
+
+    private Connection connection;
+    private PreparedStatement preparedStatement;
+    private SaloonMapper saloonMapper =  new SaloonMapper();
+
+    public SaloonRepository() throws SQLException {
+        connection = ConnectionProvider.getProvider().getOracleConnection();
+    }
+
+
+    @Override
+    public void save(Saloon saloon) throws Exception {
+
+      saloon.setSaloonId(ConnectionProvider.getProvider().getNextId("saloon-seq"));
+
+      preparedStatement = connection.prepareStatement("insert into SALOONS(saloon_id, name, address, capacity) values (?,?,?,?)");
+      preparedStatement.setInt(1, saloon.getSaloonId());
+      preparedStatement.setString(2, saloon.getName());
+      preparedStatement.setString(3, saloon.getAddress());
+      preparedStatement.setInt(4, saloon.getCapacity());
+      //preparedStatement.se
+        preparedStatement.execute();
+    }
+
+    @Override
+    public void edit(Saloon saloon) throws Exception {
+
+        preparedStatement = connection.prepareStatement("update SALOONS set SALOON_ID=?, name=?, address=?, capacity=? where saloon_id=?");
+
+        preparedStatement.setInt(1, saloon.getSaloonId());
+        preparedStatement.setString(2, saloon.getName());
+        preparedStatement.setString(3, saloon.getAddress());
+        preparedStatement.setInt(4, saloon.getCapacity());
+        preparedStatement.execute();
+
+
+    }
+
+    @Override
+    public void delete(Integer id) throws Exception {
+       preparedStatement = connection.prepareStatement("delete from SALOONS where saloon_id=?");
+
+       preparedStatement.setInt(1, id);
+       preparedStatement.execute();
+    }
+
+    @Override
+    public List<Saloon> findAll() throws Exception {
+        List<Saloon> saloonList = new ArrayList<>();
+
+        preparedStatement = connection.prepareStatement("select * from SALOONS");
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        while (resultSet.next()) {
+            Saloon saloon = saloonMapper.saloonMapper(resultSet);
+            saloonList.add(saloon);
+        }
+        return saloonList;
+    }
+
+    @Override
+    public Saloon findById(Integer id) throws Exception {
+        Saloon saloon = null;
+        preparedStatement = connection.prepareStatement("select * from SALOONS where SALOON_ID=?");
+        preparedStatement.setInt(1, id);
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        if (resultSet.next()) {
+            saloon = saloonMapper.saloonMapper(resultSet);
+        }
+        return saloon;
+    }
+
+    @Override
+    public void close() throws Exception {
+        preparedStatement.close();
+        connection.close();
+    }
 }
