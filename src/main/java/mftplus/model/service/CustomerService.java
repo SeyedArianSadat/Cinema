769130b39472429dfcebab520e4fc958a87761bd
seyedariansadat <ariansadat733@gmail.com>
2025-09-16 15:00:20 +0330

    My service  added

---
+++ src/main/java/mftplus/model/service/CustomerService.java
@@ -0,0 +1,56 @@
+package mftplus.model.service;
+
+import lombok.Getter;
+import mftplus.model.entity.Customer;
+import mftplus.model.repository.CustomerRepository;
+
+import java.util.List;
+
+public class CustomerService implements Service<Customer, Integer> {
+    @Getter
+    private static CustomerService service = new CustomerService();
+
+    private CustomerService() {
+
+    }
+
+    @Override
+    public void save(Customer customer) throws Exception {
+        try (CustomerRepository customerRepository = new CustomerRepository()) {
+            customerRepository.save(customer);
+        }
+
+    }
+
+    @Override
+    public void edit(Customer customer) throws Exception {
+        try (CustomerRepository customerRepository = new CustomerRepository()) {
+            customerRepository.edit(customer);
+        }
+
+    }
+
+    @Override
+    public void delete(Integer id) throws Exception {
+        try (CustomerRepository customerRepository = new CustomerRepository()) {
+            customerRepository.delete(id);
+        }
+
+    }
+
+    @Override
+    public List<Customer> findAll() throws Exception {
+        try (CustomerRepository customerRepository = new CustomerRepository()) {
+            return customerRepository.findAll();
+        }
+
+    }
+
+    @Override
+    public Customer findById(Integer id) throws Exception {
+        try (CustomerRepository customerRepository = new CustomerRepository()) {
+            return customerRepository.findById(id);
+        }
+
+    }
+}
---
+++ src/main/java/mftplus/model/service/EventService.java
@@ -0,0 +1,55 @@
+package mftplus.model.service;
+
+import lombok.Getter;
+import mftplus.model.entity.Event;
+import mftplus.model.repository.EventRepository;
+
+import java.util.List;
+
+public class EventService implements Service<Event, Integer> {
+    @Getter
+    private static EventService service = new EventService();
+
+    private EventService() {
+
+    }
+
+    @Override
+    public void save(Event event) throws Exception {
+        try (EventRepository eventRepository = new EventRepository()) {
+            eventRepository.save(event);
+        }
+    }
+
+    @Override
+    public void edit(Event event) throws Exception {
+        try (EventRepository eventRepository = new EventRepository()) {
+            eventRepository.edit(event);
+        }
+
+    }
+
+    @Override
+    public void delete(Integer id) throws Exception {
+        try (EventRepository eventRepository = new EventRepository()) {
+            eventRepository.delete(id);
+        }
+
+    }
+
+    @Override
+    public List<Event> findAll() throws Exception {
+        try (EventRepository eventRepository = new EventRepository()) {
+            return eventRepository.findAll();
+        }
+
+    }
+
+    @Override
+    public Event findById(Integer id) throws Exception {
+        try (EventRepository eventRepository = new EventRepository()) {
+            return eventRepository.findById(id);
+        }
+
+    }
+}
