2a4c11a4c347ab600de787aa7f7334db97c4c51c
seyedariansadat <ariansadat733@gmail.com>
2025-09-16 15:23:40 +0330

    debug

---
+++ src/test/java/ArtistTest.java
@@ -0,0 +1,5 @@
+public class ArtistTest {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/CustomerTest.java
@@ -0,0 +1,5 @@
+public class CustomerTest {
+    public static void main(String[] args) {
+
+    }
+}
