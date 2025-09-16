2a70770fc84002286e7f0b81f3345a4f1afb0f82
seyedariansadat <ariansadat733@gmail.com>
2025-09-16 14:37:27 +0330

    My Repository added

---
+++ src/main/java/mftplus/model/repository/ArtistRepository.java
@@ -0,0 +1,92 @@
+package mftplus.model.repository;
+
+import mftplus.model.entity.Artist;
+import mftplus.model.tools.ArtistMapper;
+import mftplus.model.tools.ConnectionProvider;
+
+import java.sql.*;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArtistRepository implements Repository<Artist,Integer>, AutoCloseable {
+    private final Connection connection;
+    private PreparedStatement preparedStatement;
+    private final ArtistMapper artistMapper=new ArtistMapper();
+
+    public ArtistRepository() throws Exception {
+        connection= ConnectionProvider.getProvider().getOracleConnection();
+    }
+
+    @Override
+    public void save(Artist artist) throws Exception {
+        preparedStatement=connection.prepareStatement(
+                "insert into artists (name,family,category,genre)"+
+                        " values (?,?,?,?)"
+        );
+        preparedStatement.setString(1, artist.getName());
+        preparedStatement.setString(2, artist.getFamily());
+        preparedStatement.setString(3, artist.getCategory());
+        preparedStatement.setString(4, artist.getGenre());
+        preparedStatement.executeQuery();
+
+    }
+
+    @Override
+    public void edit(Artist artist) throws Exception {
+        preparedStatement=connection.prepareStatement(
+                "update artists set name=?,family=?,category=?,genre=? where artist_id=?"
+        );
+        preparedStatement.setString(1, artist.getName());
+        preparedStatement.setString(2, artist.getFamily());
+        preparedStatement.setString(3, artist.getCategory());
+        preparedStatement.setString(4, artist.getGenre());
+        preparedStatement.executeQuery();
+
+    }
+
+    @Override
+    public void delete(Integer id) throws Exception {
+        preparedStatement=connection.prepareStatement(
+                "delete from artists where artist_id=?"
+        );
+        preparedStatement.setInt(1, id);
+        preparedStatement.executeQuery();
+
+    }
+
+    @Override
+    public List<Artist> findAll() throws Exception {
+        List<Artist> artistList=new ArrayList<>();
+
+        preparedStatement=connection.prepareStatement("select * from artists order by artist_id");
+        ResultSet resultSet=preparedStatement.executeQuery();
+
+        while (resultSet.next()) {
+            Artist artist=artistMapper.artistMapper(resultSet);
+            artistList.add(artist);
+        }
+        return artistList;
+
+    }
+
+    @Override
+    public Artist findById(Integer id) throws Exception {
+        Artist artist=null;
+
+        preparedStatement=connection.prepareStatement("select * from artists where artist_id=?");
+        preparedStatement.setInt(1, id);
+        ResultSet resultSet=preparedStatement.executeQuery();
+        if (resultSet.next()) {
+            artist=artistMapper.artistMapper(resultSet);
+        }
+        return artist;
+    }
+
+    @Override
+    public void close() throws Exception {
+        preparedStatement.close();
+        connection.close();
+
+    }
+}
---
+++ src/main/java/mftplus/model/repository/CustomerRepository.java
@@ -0,0 +1,89 @@
+package mftplus.model.repository;
+
+import mftplus.model.entity.Customer;
+import mftplus.model.tools.ConnectionProvider;
+import mftplus.model.tools.CustomerMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.*;
+
+public class CustomerRepository implements Repository<Customer,Integer>,AutoCloseable {
+    private final Connection connection;
+    private PreparedStatement preparedStatement;
+    private final CustomerMapper customerMapper=new CustomerMapper();
+
+    public  CustomerRepository() throws Exception {
+        connection= ConnectionProvider.getProvider().getOracleConnection();
+    }
+    @Override
+    public void save(Customer customer) throws Exception {
+        preparedStatement=connection.prepareStatement(
+                "insert into customers(name,family,phone_number,age)" +
+                "values(?,?,?,?)"
+        );
+        preparedStatement.setString(1, customer.getName());
+        preparedStatement.setString(2, customer.getFamily());
+        preparedStatement.setString(3, customer.getPhoneNumber());
+        preparedStatement.setInt(4, customer.getAge());
+        preparedStatement.executeQuery();
+
+    }
+
+    @Override
+    public void edit(Customer customer) throws Exception {
+        preparedStatement=connection.prepareStatement(
+                "update customers set name=?,family=?,phone_number=?,age=? where customer_id=?"
+        );
+        preparedStatement.setString(1, customer.getName());
+        preparedStatement.setString(2, customer.getFamily());
+        preparedStatement.setString(3, customer.getPhoneNumber());
+        preparedStatement.setInt(4, customer.getAge());
+        preparedStatement.executeQuery();
+
+    }
+
+    @Override
+    public void delete(Integer id) throws Exception {
+        preparedStatement=connection.prepareStatement(
+                "delete from customers where customer_id=?"
+        );
+        preparedStatement.setInt(1, id);
+        preparedStatement.executeQuery();
+
+    }
+
+    @Override
+    public List<Customer> findAll() throws Exception {
+        List<Customer> customerList = new ArrayList<>();
+
+        preparedStatement = connection.prepareStatement("select * from customers order by customer_id");
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        while (resultSet.next()) {
+            Customer customer = customerMapper.customerMapper(resultSet);
+            customerList.add(customer);
+        }
+        return customerList;
+    }
+
+    @Override
+    public Customer findById(Integer id) throws Exception {
+       Customer customer = null;
+
+       preparedStatement=connection.prepareStatement("select * from customers where customer_id=?");
+       preparedStatement.setInt(1, id);
+       ResultSet resultSet = preparedStatement.executeQuery();
+       if (resultSet.next()) {
+           customer = customerMapper.customerMapper(resultSet);
+       }
+       return customer;
+    }
+
+    @Override
+    public void close() throws Exception {
+        preparedStatement.close();
+        connection.close();
+
+    }
+}
