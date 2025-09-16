873983b27ff97475c2048f28c0019926e8bcee26
romina <rominamirfattahi13@gmail.com>
2025-09-16 15:17:13 +0330

    romina's files added

--- src/main/java/mftplus/model/tools/SaloonMapper.java
+++ src/main/java/mftplus/model/tools/SaloonMapper.java
@@ -1,4 +1,19 @@
 package mftplus.model.tools;
 
+import mftplus.model.entity.Saloon;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class SaloonMapper {
+    public Saloon saloonMapper(ResultSet resultSet) throws SQLException {
+        return Saloon
+                .builder()
+                .saloonId(resultSet.getInt("saloon-id"))
+                .name(resultSet.getString("saloon-name"))
+                .address(resultSet.getString("address"))
+                .capacity(resultSet.getInt("capacity"))
+                // .seatList(?)
+                //.manager(?)
+                .build();
+    }
 }
