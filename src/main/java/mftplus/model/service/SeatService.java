f0078a325fcfe5cdab04b62cd2d9b4cb4f7fab5c
romina <rominamirfattahi13@gmail.com>
2025-09-16 20:46:10 +0330

    romina's files added

--- src/main/java/mftplus/model/service/SeatService.java
+++ src/main/java/mftplus/model/service/SeatService.java
@@ -1,4 +1,56 @@
 package mftplus.model.service;
 
-public class SeatService {
+import lombok.Getter;
+import mftplus.model.entity.Seat;
+import mftplus.model.repository.SeatRepository;
+
+import java.util.Collections;
+import java.util.List;
+
+public class SeatService implements Service<Seat, Integer>{
+
+    @Getter
+    private static SeatService service = new SeatService();
+
+
+    private SeatService(){
+
+    }
+
+
+
+    @Override
+    public void save(Seat seat) throws Exception {
+        try(SeatRepository seatRepository = new SeatRepository()) {
+            seatRepository.save(seat);
+        }
+    }
+
+    @Override
+    public void edit(Seat seat) throws Exception {
+        try(SeatRepository seatRepository = new SeatRepository()) {
+            seatRepository.edit(seat);
+        }
+    }
+
+    @Override
+    public void delete(Integer id) throws Exception {
+        try(SeatRepository seatRepository = new SeatRepository()) {
+            seatRepository.delete(id);
+        }
+    }
+
+    @Override
+    public List<Seat> findAll() throws Exception {
+        try(SeatRepository seatRepository = new SeatRepository()) {
+           return seatRepository.findAll();
+        }
+    }
+
+    @Override
+    public Seat findById(Integer id) throws Exception {
+        try(SeatRepository seatRepository = new SeatRepository()) {
+            return seatRepository.findById(id);
+        }
+    }
 }
