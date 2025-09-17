fd04350d755ed5cf25503291f9bb2481f778eeb3
seyedariansadat <ariansadat733@gmail.com>
2025-09-17 15:45:51 +0330

    debug

--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -1,5 +1,14 @@
+import mftplus.model.entity.Event;
+
+import java.time.LocalDateTime;
+
 public class EventTest {
     public static void main(String[] args) {
+Event event=Event.builder().eventId(1).title("fast").description("fast").eventStartTime(LocalDateTime.of(2020, 10, 10, 10, 10))
+.eventEndTime(LocalDateTime.of(2020, 10, 10, 10, 10)).build();
 
-    }
+        System.out.println("success");
+
+
+}
 }
