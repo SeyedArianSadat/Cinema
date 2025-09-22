90819498ee048681e815f35376c1f033e70d4e97
seyedariansadat <ariansadat733@gmail.com>
2025-09-22 17:38:54 +0330

    Connection pool added

--- src/main/java/mftplus/model/tools/ConnectionProvider.java
+++ src/main/java/mftplus/model/tools/ConnectionProvider.java
@@ -1,40 +1,50 @@
 package mftplus.model.tools;
 
 import lombok.Getter;
+import org.apache.commons.dbcp2.BasicDataSource;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.Duration;
+
 
 public class ConnectionProvider {
     @Getter
-    private final static ConnectionProvider provider = new ConnectionProvider();
+    private static final ConnectionProvider provider = new ConnectionProvider();
 
+
+    private static final BasicDataSource dataSource = new BasicDataSource();
     private ConnectionProvider() {
+        dataSource.setDriverClassName("org.hsqldb.jdbcDriver");
+        dataSource.setUrl("jdbc:oracle:thin:@localhost:1521:xe");
+        dataSource.setUsername("javase");
+        dataSource.setPassword("javase123");
+
+        dataSource.setMaxIdle(4);
+        dataSource.setMaxIdle(8);
+        dataSource.setMaxTotal(16);
+
+        dataSource.setMaxWait(Duration.ofSeconds(10));
+
+    }
+    public Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+    public int getNextId(String sequenceName) throws SQLException {
+        String sql = "SELECT " + sequenceName + ".nextval AS NEXT_ID FROM dual" ;
+        try (Connection connection= getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql);
+             ResultSet resultSet = preparedStatement.executeQuery()){
+                 if (resultSet.next()) {
+                     return resultSet.getInt("next_id");
+                 }else  throw   new SQLException("Sequence " + sequenceName + " did not return a value");
+        }
+
+
     }
 
-    public Connection getOracleConnection() throws SQLException {
-        return DriverManager.getConnection(
-                "jdbc:oracle:thin:@localhost:1521:XE",
-                "javase",
-                "java123"
-        );
+
     }
 
-    public int getNextId(String sequenceName) throws Exception{
-
-        ResultSet resultSet = getOracleConnection().prepareStatement(String.format("select %s.nextval as NEXT_ID from dual", sequenceName)).executeQuery();
-        resultSet.next();
-        return resultSet.getInt("NEXT_ID");
-    }
-
-    public Connection getH2Connection() throws SQLException {
-//        Class.forName("org.h2.Driver");
-        return DriverManager.getConnection(
-                "jdbc:h2:file:./db/mft_class;AUTO_SERVER=TRUE;MODE=Oracle",
-                "sa",
-                ""
-        );
-    }
-}
