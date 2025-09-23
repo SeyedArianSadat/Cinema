90548775727a514903c48606c957e94bc1a4011c
Hedie-khodaei <hedi2002saniyani@gmail.com>
2025-09-23 14:30:05 +0330

    add

--- src/test/java/TicketTest.java
+++ src/test/java/TicketTest.java
@@ -1,21 +1,32 @@
+import mftplus.model.entity.*;
+import mftplus.model.service.TicketService;
 
+import java.time.LocalDateTime;
 
 public class TicketTest {
+    public static void main(String[] args) throws  Exception {
+        Event event = Event.builder()
+                .eventId(1).build();
+        Customer customer = Customer.builder()
+                .customerId(1).build();
+        Seat seat = Seat.builder()
+                .seatId(1).build();
+        Payment payment = Payment.builder()
+                .paymentId(1).build();
 
-}
-/*
-    public static void main(String[] args) throws Exception {
-        Ticket ticket = Ticket
-                .builder()
-                .ticketId(1)
-                .ticketTime(LocalDateTime.now())
-                .build();
-        try (TicketRepository ticketRepository = new TicketRepository()) {
-            ticketRepository.save(ticket);
 
-        }
-
+        Ticket ticket =
+                Ticket.builder()
+                        .ticketId(1)
+                        .event(event)
+                        .customer(customer)
+                        .seat(seat)
+                        .payment(payment)
+                        .ticketTime(LocalDateTime.of(2002, 12, 12, 12,12))
+                        .build();
+        TicketService.getService().save(ticket);
     }
 
- */
+}
+
 
